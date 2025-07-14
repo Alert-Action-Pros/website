4ffddb89f962f4d20aba6da669b5e8ed7a422b17
RAJAT MISHRA <mrajat099@gmail.com>
2025-07-14 20:59:53 +0530

    upgrades

--- script.js
+++ script.js
@@ -154,13 +154,13 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      navbar.classList.add('gradient-bg');
-    } else {
-      navbar.classList.remove('gradient-bg');
-    }
-  });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   window.addEventListener('scroll', function() {
+//     const navbar = document.getElementById('navbar');
+//     if (window.scrollY > 50) {
+//       navbar.classList.add('gradient-bg');
+//     } else {
+//       navbar.classList.remove('gradient-bg');
+//     }
+//   });
+// });
